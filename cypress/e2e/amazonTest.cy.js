4498cbab32185466aafe28e00bc714de875d412a
Pablo Arzu <jptomas38@gmail.com>
2023-03-09 10:19:47 -0600

    Smoke Test

--- cypress/e2e/amazonTest.cy.js
+++ cypress/e2e/amazonTest.cy.js
@@ -1,5 +1,11 @@
 describe('Pruebas Amazon', () => {
 
-  
+  const url = 'https://www.amazon.com/'
+
+  it(' debe retornar el tercer elemento de la segunda pagina de la busqueda por "alexa" ', () => {
+
+    cy.visit(url)
+
+  })
 
 })
